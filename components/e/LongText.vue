b0f349face4a3598bbff1b3311631b981c3bac54
Elvino <elvino_tan@yahoo.com>
2021-05-14 23:58:40 +0700

    Update text component

--- components/e/LongText.vue
+++ components/e/LongText.vue
@@ -1,26 +1,35 @@
 <template>
   <div>
-    <label class="text-gray-600 text-sm font-bold" :for="id">
-      {{ label }}
-      <span class="text-red-500">{{ required ? ' *' : '' }}</span>
-    </label>
-    <input
-      v-if="show"
-      :id="id"
-      :placeholder="placeholder"
-      type="text"
-      :value="value"
-      :maxlength="maxlength"
-      :disabled="disabled"
-      :required="required"
-      class="px-1 py-1 uppercase placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-xs border-0 shadow outline-none focus:outline-none focus:ring-1 ring-gray-500 w-full"
-      @input="_event"
-      @blur="_event"
-    />
-
-    <p class="text-red-500 text-right text-xs italic">
-      {{ errors.length > 0 ? errors[0] : '' }}
-    </p>
+    <div v-if="show">
+      <span
+        class="flex text-xs rounded border-0 outline-none ring-1 ring-gray-500"
+      >
+        <span
+          class="bg-gray-300 font-bold text-left rounded-l text-sm text-gray-800 w-auto p-1"
+        >
+          {{ label ? label.replaceAll(' ', '&nbsp;') : ''
+          }}{{ required ? '&nbsp;*' : '' }}
+        </span>
+        <input
+          :id="id"
+          type="text"
+          :placeholder="placeholder"
+          :value="value"
+          :maxlength="maxlength"
+          :disabled="disabled"
+          :required="required"
+          class="field text-sm text-gray-600 rounded-r p-1 px-1 text-sm w-full outline-none uppercase placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white"
+          @input="_input"
+          @blur="_blur"
+        />
+      </span>
+      <p v-if="hasError()" class="text-red-500 text-right text-xs italic">
+        {{ errors[0] }}
+      </p>
+      <p v-else class="text-right text-xs italic">
+        {{ `${value ? value.length : 0} / ${maxlength}` }}
+      </p>
+    </div>
   </div>
 </template>
 <script>
@@ -39,20 +48,20 @@ export default {
   },
   data() {
     return {
-      localValue: this.value,
       errors: [],
     }
   },
   methods: {
-    _event(evt) {
-      this.localValue = evt.target.value.toUpperCase()
-      if (evt.type === 'blur') {
-        // Blur melakukan trimming yang artinya ubah data, maka infoin juga input
-        this.localValue = this.localValue.trim()
-        this.$emit('input', this.localValue)
-      }
-      this.$emit(evt.type, this.localValue)
-      this.validate()
+    _input(event) {
+      const value = event.target.value.toUpperCase()
+      this.$emit(event.type, value)
+      this.$nextTick(this.validate)
+    },
+    _blur(event) {
+      const value = event.target.value.toUpperCase().trim()
+      this.$emit('input', value)
+      this.$emit(event.type, value)
+      this.$nextTick(this.validate)
     },
     metaData() {
       return {
@@ -71,16 +80,16 @@ export default {
       this.clearError()
 
       // General validation base on props
-      if (this.required && !this.localValue) {
+      if (this.required && !this.value) {
         this.errors.push(`${this.label} is required`)
       }
-      if (this.localValue && this.localValue.length > this.maxlength) {
+      if (this.value && this.value.length > this.maxlength) {
         this.errors.push(`${this.label} is exceeded`)
       }
 
       // add business runtime validation
       if (this.vruntime) {
-        const error = this.vruntime(this.localValue)
+        const error = this.vruntime(this.value)
         if (error) this.errors.push(error)
       }
 
