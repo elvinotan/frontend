4566aaa058327fcdfa9c7bf4ad15dcf866582774
Elvino <elvino_tan@yahoo.com>
2021-05-17 19:07:18 +0700

    commit

--- components/e/Boolean.vue
+++ components/e/Boolean.vue
@@ -21,7 +21,7 @@
         </span>
         <select
           :id="id"
-          :value="lvalue"
+          v-model="lvalue"
           :disabled="disabled"
           :required="required"
           class="field text-sm text-gray-800 rounded-r p-1 px-1 text-sm w-full outline-none uppercase placeholder-blueGray-300 relative"
