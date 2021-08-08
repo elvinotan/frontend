200632bceb3aed9767dc93ed01dbdaab5afc1102
Elvino <elvino_tan@yahoo.com>
2021-08-08 23:40:24 +0700

    add blank component

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-    <ECard label="Login" :disabled="false" :show="true">
-      <EForm id="login" ref="login">
-        <ECol>
+    <ECol :col="3">
+      <EBlank />
+      <ECard label="Login" :disabled="false" :show="true">
+        <EForm id="login" ref="login">
           <EText id="username" ref="username" v-model="model.username" label="Username" type="short" :required="true" :disabled="ui.username.disabled" :show="true" />
           <EPassword id="password" ref="password" v-model="model.password" label="Password" :required="true" :disabled="ui.password.disabled" :show="true" />
-        </ECol>
-      </EForm>
-      <EButton id="signIn" ref="signIn" label="Sign In" :disabled="ui.signIn.disabled" color="green" :show="true" @click="signIn" />
-    </ECard>
+        </EForm>
+        <EButton id="signIn" ref="signIn" label="Sign In" :disabled="ui.signIn.disabled" color="green" :show="true" @click="signIn" />
+      </ECard>
+      <EBlank />
+    </ECol>
     <EMessage id="message" ref="message" />
   </div>
 </template>
