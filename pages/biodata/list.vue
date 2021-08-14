5b70857ce04e30cf7e2c84f118ba82819e2bb82b
Elvino <elvino_tan@yahoo.com>
2021-08-14 19:02:49 +0700

    rapihkan list

--- pages/biodata/list.vue
+++ pages/biodata/list.vue
@@ -1,60 +1,58 @@
 <template>
-  <EForm id="customerForm" ref="customerForm">
-    <ECol :gap-y="3">
-      <EFilter id="customerFilter" ref="customerFilter" title="Search Customer" :disabled="ui.filter.disabled" @Clear="clear" @Search="search">
-        <ECol :col="4">
-          <EText id="filterFatherName" v-model="filter.fatherName" label="Nama Ayah" placeholder="Input Full Name" type="short" :disabled="ui.fatherName.disabled" />
-          <ENumber id="filterFatherNik" v-model="filter.fatherNik" label="NIK" placeholder="Input NIK" type="short" :disabled="ui.fatherNik.disabled" />
-          <EDate id="filterFatherDateOfBirth" v-model="filter.fatherDateOfBirth" label="Tgl Lahir" :disabled="ui.fatherDateOfBirth.disabled" />
-          <ELookup id="filterFatherReligion" v-model="filter.fatherReligion" label="Agama" :disabled="ui.fatherReligion.disabled" lookup-group="DEMO_RELIGION" />
-          <EText id="filterMotherName" v-model="filter.motherName" label="Nama Ibu" placeholder="Input Full Name" type="short" :disabled="ui.motherName.disabled" />
-          <ENumber id="filterMotherNik" v-model="filter.motherNik" label="NIK" placeholder="Input NIK" type="short" :disabled="ui.motherNik.disabled" />
-          <EDate id="filterFatherDateOfBirth" v-model="filter.motherDateOfBirth" label="Tgl Lahir" :disabled="ui.motherDateOfBirth.disabled" />
-          <ELookup id="filterMotherReligion" v-model="filter.motherReligion" label="Agama" :disabled="ui.motherReligion.disabled" lookup-group="DEMO_RELIGION" />
-        </ECol>
-      </EFilter>
-      <EServerPagination
-        id="pagingFamily"
-        ref="pagingFamily"
-        label="List Of Family"
-        picker="pagingFamily"
-        :auto-load="true"
-        :add-new-data="addNewData"
-        :disabled-action="disabledAction"
-        :filter="filter"
-        :initial-sort-by="[{ field: 'fatherName', type: 'asc' }]"
-        :actions="[
-          { label: 'Edit', emit: 'ubah' },
-          { label: 'View', emit: 'lihat' },
-        ]"
-        :columns="[
-          { label: 'Nama Ayah', field: 'fatherName', sortable: true, width: '100px', tooltip: 'Father Name', type: 'text' },
-          { label: 'NIK', field: 'fatherNik', sortable: true, width: '100px', tooltip: 'NIK', type: 'text' },
-          { label: 'Kelamin', field: 'fatherSex', sortable: true, width: '100px', tooltip: 'Kelamin', type: 'lookup', reference: 'DEMO_SEX' },
-          { label: 'Tmpt Lahir', field: 'fatherPlaceOfBirth', sortable: true, width: '100px', tooltip: 'Tempat Lahir', type: 'text' },
-          { label: 'Tgl Lahir', field: 'fatherDateOfBirth', sortable: true, width: '100px', tooltip: 'Tanggal Lahir', type: 'date' },
-          { label: 'Agama', field: 'fatherReligion', sortable: true, width: '100px', tooltip: 'Agama', type: 'lookup', reference: 'DEMO_RELIGION' },
-          { label: 'Edukasi', field: 'fatherEducation', sortable: true, width: '100px', tooltip: 'Edukasi', type: 'lookup', reference: 'DEMO_EDUCATION' },
-          { label: 'Pekerjaan', field: 'fatherJob', sortable: true, width: '100px', tooltip: 'Pekerjaan', type: 'lookup', reference: 'DEMO_JOB' },
-          { label: 'Nama Ibu', field: 'motherName', sortable: true, width: '100px', tooltip: 'Father Name', type: 'text' },
-          { label: 'NIK', field: 'motherNik', sortable: true, width: '100px', tooltip: 'NIK', type: 'text' },
-          { label: 'Kelamin', field: 'motherSex', sortable: true, width: '100px', tooltip: 'Kelamin', type: 'lookup', reference: 'DEMO_SEX' },
-          { label: 'Tmpt Lahir', field: 'motherPlaceOfBirth', sortable: true, width: '100px', tooltip: 'Tempat Lahir', type: 'text' },
-          { label: 'Tgl Lahir', field: 'motherDateOfBirth', sortable: true, width: '100px', tooltip: 'Tanggal Lahir', type: 'date' },
-          { label: 'Agama', field: 'motherReligion', sortable: true, width: '100px', tooltip: 'Agama', type: 'lookup', reference: 'DEMO_RELIGION' },
-          { label: 'Edukasi', field: 'motherEducation', sortable: true, width: '100px', tooltip: 'Edukasi', type: 'lookup', reference: 'DEMO_EDUCATION' },
-          { label: 'Pekerjaan', field: 'motherJob', sortable: true, width: '100px', tooltip: 'Pekerjaan', type: 'lookup', reference: 'DEMO_JOB' },
-        ]"
-        @RowClick="rowClick"
-        @lihat="lihat"
-        @ubah="ubah"
-      />
-    </ECol>
+  <ECol :gap-y="3">
+    <EFilter id="customerFilter" ref="customerFilter" title="Search Customer" :disabled="ui.filter.disabled" @Clear="clear" @Search="search">
+      <ECol :col="4">
+        <EText id="filterFatherName" v-model="filter.fatherName" label="Nama Ayah" placeholder="Input Full Name" type="short" :disabled="ui.fatherName.disabled" />
+        <ENumber id="filterFatherNik" v-model="filter.fatherNik" label="NIK" placeholder="Input NIK" type="short" :disabled="ui.fatherNik.disabled" />
+        <EDate id="filterFatherDateOfBirth" v-model="filter.fatherDateOfBirth" label="Tgl Lahir" :disabled="ui.fatherDateOfBirth.disabled" />
+        <ELookup id="filterFatherReligion" v-model="filter.fatherReligion" label="Agama" :disabled="ui.fatherReligion.disabled" lookup-group="DEMO_RELIGION" />
+        <EText id="filterMotherName" v-model="filter.motherName" label="Nama Ibu" placeholder="Input Full Name" type="short" :disabled="ui.motherName.disabled" />
+        <ENumber id="filterMotherNik" v-model="filter.motherNik" label="NIK" placeholder="Input NIK" type="short" :disabled="ui.motherNik.disabled" />
+        <EDate id="filterFatherDateOfBirth" v-model="filter.motherDateOfBirth" label="Tgl Lahir" :disabled="ui.motherDateOfBirth.disabled" />
+        <ELookup id="filterMotherReligion" v-model="filter.motherReligion" label="Agama" :disabled="ui.motherReligion.disabled" lookup-group="DEMO_RELIGION" />
+      </ECol>
+    </EFilter>
+    <EServerPagination
+      id="pagingFamily"
+      ref="pagingFamily"
+      label="List Of Family"
+      picker="pagingFamily"
+      :auto-load="true"
+      :add-new-data="addNewData"
+      :disabled-action="disabledAction"
+      :filter="filter"
+      :initial-sort-by="[{ field: 'fatherName', type: 'asc' }]"
+      :actions="[
+        { label: 'Edit', emit: 'ubah' },
+        { label: 'View', emit: 'lihat' },
+      ]"
+      :columns="[
+        { label: 'Nama Ayah', field: 'fatherName', sortable: true, width: '100px', tooltip: 'Father Name', type: 'text' },
+        { label: 'NIK', field: 'fatherNik', sortable: true, width: '100px', tooltip: 'NIK', type: 'text' },
+        { label: 'Kelamin', field: 'fatherSex', sortable: true, width: '100px', tooltip: 'Kelamin', type: 'lookup', reference: 'DEMO_SEX' },
+        { label: 'Tmpt Lahir', field: 'fatherPlaceOfBirth', sortable: true, width: '100px', tooltip: 'Tempat Lahir', type: 'text' },
+        { label: 'Tgl Lahir', field: 'fatherDateOfBirth', sortable: true, width: '100px', tooltip: 'Tanggal Lahir', type: 'date' },
+        { label: 'Agama', field: 'fatherReligion', sortable: true, width: '100px', tooltip: 'Agama', type: 'lookup', reference: 'DEMO_RELIGION' },
+        { label: 'Edukasi', field: 'fatherEducation', sortable: true, width: '100px', tooltip: 'Edukasi', type: 'lookup', reference: 'DEMO_EDUCATION' },
+        { label: 'Pekerjaan', field: 'fatherJob', sortable: true, width: '100px', tooltip: 'Pekerjaan', type: 'lookup', reference: 'DEMO_JOB' },
+        { label: 'Nama Ibu', field: 'motherName', sortable: true, width: '100px', tooltip: 'Father Name', type: 'text' },
+        { label: 'NIK', field: 'motherNik', sortable: true, width: '100px', tooltip: 'NIK', type: 'text' },
+        { label: 'Kelamin', field: 'motherSex', sortable: true, width: '100px', tooltip: 'Kelamin', type: 'lookup', reference: 'DEMO_SEX' },
+        { label: 'Tmpt Lahir', field: 'motherPlaceOfBirth', sortable: true, width: '100px', tooltip: 'Tempat Lahir', type: 'text' },
+        { label: 'Tgl Lahir', field: 'motherDateOfBirth', sortable: true, width: '100px', tooltip: 'Tanggal Lahir', type: 'date' },
+        { label: 'Agama', field: 'motherReligion', sortable: true, width: '100px', tooltip: 'Agama', type: 'lookup', reference: 'DEMO_RELIGION' },
+        { label: 'Edukasi', field: 'motherEducation', sortable: true, width: '100px', tooltip: 'Edukasi', type: 'lookup', reference: 'DEMO_EDUCATION' },
+        { label: 'Pekerjaan', field: 'motherJob', sortable: true, width: '100px', tooltip: 'Pekerjaan', type: 'lookup', reference: 'DEMO_JOB' },
+      ]"
+      @RowClick="rowClick"
+      @lihat="lihat"
+      @ubah="ubah"
+    />
     <EMessage id="customerMessage" ref="customerMessage" />
-  </EForm>
+  </ECol>
 </template>
 <script>
-const emptyFilter = {
+const empty = {
   fatherName: null,
   fatherNik: null,
   fatherDateOfBirth: null,
@@ -69,7 +67,7 @@ export default {
   name: 'List',
   data() {
     return {
-      filter: { ...emptyFilter },
+      filter: { ...empty },
       ui: {
         filter: { disabled: false },
         fatherName: { disabled: false },
@@ -92,10 +90,10 @@ export default {
     },
     disabledAction(label, data) {
       if (label.emit === 'ubah') {
-        return data.row.merried
+        return false
       }
       if (label.emit === 'lihat') {
-        return data.row.loanPct > 50
+        return false
       }
       return false
     },
@@ -109,7 +107,7 @@ export default {
       this.$nav.to({ path: '/biodata/entry', familyId: data.row.id, from: 'familyList', view: true })
     },
     clear() {
-      this.filter = { ...emptyFilter }
+      this.filter = { ...empty }
     },
     async search() {
       const { valid } = this.$wrapper.validate(this.$refs.customerFilter)
